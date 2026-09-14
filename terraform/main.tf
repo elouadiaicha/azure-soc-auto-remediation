e0a4d53fb6a65bd2759daae1aaf2b4e450f9e10d
A-benslimane <benslimane201440@gmail.com>
2026-09-14 11:50:16 +0200

    feat: add Logic App remediation with managed identity

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
 data "azurerm_resource_group" "soc" {
   name = var.resource_group_name
 }
+
+module "remediation" {
+  source = "./modules/remediation"
+
+  resource_group_name = var.resource_group_name
+  location            = data.azurerm_resource_group.soc.location
+  nsg_name            = var.nsg_name
+  logic_app_name      = "logic-soc-remediation"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,23 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.soc.name
+  value = data.azurerm_resource_group.soc.name
 }
 
 output "vnet_name" {
-  value = azurerm_virtual_network.soc.name
+  value = var.vnet_name
 }
 
 output "nsg_name" {
-  value = azurerm_network_security_group.soc.name
+  value = var.nsg_name
+}
+
+output "logic_app_name" {
+  value = module.remediation.logic_app_name
+}
+
+output "logic_app_id" {
+  value = module.remediation.logic_app_id
+}
+
+output "logic_app_managed_identity_principal_id" {
+  value = module.remediation.managed_identity_principal_id
 }
