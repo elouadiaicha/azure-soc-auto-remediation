d185f63649358ca4a517707fc9c503f5eb92bc7f
malikcherfi <malikcherfi@gmail.com>
2026-09-14 12:02:32 +0200

    refactor: replace data source with variable references for VNet and NSG

--- terraform/modules/infrastructure/main.tf
+++ terraform/modules/infrastructure/main.tf
@@ -1,19 +1,14 @@
-# Resource Group déjà existant
-data "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
-}
-
 # VNet
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
 }
 
 # NSG qui servira à la simulation de l'incident
 resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
