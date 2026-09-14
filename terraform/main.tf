bc5b1818cd0268980c996d192b888a1560fe6d7e
malikcherfi <malikcherfi@gmail.com>
2026-09-14 12:02:20 +0200

    refactor: consolidate resource group data usage in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,30 @@
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
 module "infrastructure" {
   source = "./modules/infrastructure"
 
-  resource_group_name = var.resource_group_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   vnet_name           = var.vnet_name
   nsg_name            = var.nsg_name
-}
-data "azurerm_resource_group" "soc" {
-  name = var.resource_group_name
 }
 
 module "remediation" {
   source = "./modules/remediation"
 
-  resource_group_name = var.resource_group_name
-  location            = data.azurerm_resource_group.soc.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   nsg_name            = var.nsg_name
   logic_app_name      = "logic-soc-remediation"
-  
+
+}
+
 module "sentinel" {
   source = "./modules/sentinel"
 
-  resource_group_name  = data.azurerm_resource_group.soc.name
-  location             = var.location
-  subscription_id      = var.subscription_id
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  subscription_id     = var.subscription_id
 }
