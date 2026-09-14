273e3da3d18ae9154e388bfeea3e666ed96fb076
malikcherfi <malikcherfi@gmail.com>
2026-09-14 12:21:04 +0200

    feat: add logic app ID and reference to remediation module in sentinel configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,4 +27,6 @@ module "sentinel" {
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
   subscription_id     = var.subscription_id
+  logic_app_id        = module.remediation.logic_app_id
+  logic_app           = module.remediation.logic_app
 }
